447ddee61ca58cc87cf088fca3489939a5013db7
Neil Bauman <nbauman@rocketmail.com>
2025-09-25 21:34:37 -0300

    Create nav.ts

---
+++ lib/ui/nav.ts
@@ -0,0 +1,29 @@
+// /lib/ui/nav.ts
+export const NAV = [
+  {
+    label: "General",
+    items: [
+      { title: "Dashboard", href: "/dashboard" },
+      { title: "About", href: "/about" }
+    ]
+  },
+  {
+    label: "SSC Configuration",
+    items: [
+      { title: "Primary Framework Editor", href: "/configuration/primary" }
+    ]
+  },
+  {
+    label: "Operations",
+    items: [
+      { title: "Country Configuration", href: "/configuration" },
+      { title: "SSC Instances", href: "/instances" }
+    ]
+  },
+  {
+    label: "Admin",
+    items: [
+      { title: "Users & Auth", href: "/admin" }
+    ]
+  }
+];
