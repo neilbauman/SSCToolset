19b427ab0e57b2fb5790a770e0a7e07f4a9a16b8
Neil Bauman <nbauman@rocketmail.com>
2025-09-25 21:36:26 -0300

    Update route.ts

--- app/api/framework/versions/[id]/items/route.ts
+++ app/api/framework/versions/[id]/items/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 import { getVersionTree } from "@/lib/services/framework";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
   try {
-    const data = await getVersionTree(params.id);
+    const data = await getVersionTree(context.params.id);
     return NextResponse.json({ data });
   } catch (e: any) {
     return NextResponse.json({ error: e.message }, { status: 400 });
