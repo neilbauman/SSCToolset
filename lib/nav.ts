d79fb3b1b03acae2cc8e9bd7d9b8ad7133e0ea62
Neil Bauman <nbauman@rocketmail.com>
2025-09-26 08:14:10 -0300

    Update nav.ts

--- lib/nav.ts
+++ lib/nav.ts
@@ -3,27 +3,22 @@ export const NAV = [
     label: "General",
     items: [
       { title: "Dashboard", href: "/dashboard" },
-      { title: "About", href: "/about" }
-    ]
+      { title: "About", href: "/about" },
+    ],
   },
   {
     label: "SSC Configuration",
-    items: [
-      { title: "Primary Framework Editor", href: "/configuration/primary" }
-      // Future: { title: "Comprehensive Framework Editor", href: "/configuration/comprehensive" }
-    ]
+    items: [{ title: "Primary Framework Editor", href: "/configuration/primary" }],
   },
   {
     label: "Operations",
     items: [
       { title: "Country Configuration", href: "/configuration" },
-      { title: "SSC Instances", href: "/instances" }
-    ]
+      { title: "SSC Instances", href: "/instances" },
+    ],
   },
   {
     label: "Admin",
-    items: [
-      { title: "Users & Auth", href: "/admin" }
-    ]
-  }
+    items: [{ title: "Users & Auth", href: "/admin" }],
+  },
 ];
