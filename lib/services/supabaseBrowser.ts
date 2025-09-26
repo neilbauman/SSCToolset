331115a05c87b7451480f7c2d66249fbb1054f6b
Neil Bauman <nbauman@rocketmail.com>
2025-09-26 07:17:43 -0300

    Update supabaseBrowser.ts

--- lib/services/supabaseBrowser.ts
+++ lib/services/supabaseBrowser.ts
@@ -2,7 +2,11 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env["NEXT_PUBLIC_SUPABASE_URL"]!;
-const anon = process.env["NEXT_PUBLIC_SUPABASE_ANON_KEY"]!;
+// Env vars are now strongly typed via env.d.ts
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!url) throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL");
+if (!anon) throw new Error("Missing NEXT_PUBLIC_SUPABASE_ANON_KEY");
 
 export const supabaseBrowser = createClient(url, anon);
