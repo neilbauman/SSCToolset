b1f7f2d3f498b72f05ce2725220b7cd1ddc57290
Neil Bauman <nbauman@rocketmail.com>
2025-10-01 14:39:28 -0300

    Update populationTemplate.ts

--- lib/templates/populationTemplate.ts
+++ lib/templates/populationTemplate.ts
@@ -1,21 +1,14 @@
 import Papa from "papaparse";
 
-export function generatePopulationTemplate() {
-  const template = [
-    {
-      pcode: "PH0102802001",
-      population: "12345",
-      households: "2500",
-      dataset_date: "2020-01-01",
-    },
-    {
-      pcode: "PH0102802002",
-      population: "6789",
-      households: "1300",
-      dataset_date: "2020-01-01",
-    },
+export function generatePopulationTemplate(): Blob {
+  const example = [
+    { pcode: "PH0101", population: 100000, last_updated: "2020-01-01" },
+    { pcode: "PH0102", population: 75000, last_updated: "2020-01-01" },
   ];
 
-  const csv = Papa.unparse(template);
+  const csv = Papa.unparse(example, {
+    columns: ["pcode", "population", "last_updated"],
+  });
+
   return new Blob([csv], { type: "text/csv;charset=utf-8;" });
 }
