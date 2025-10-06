3332b6cf09a6ad9f8664af8f5c17750dd2e2fffd
Neil Bauman <nbauman@rocketmail.com>
2025-10-06 11:13:57 -0300

    Create country.ts

---
+++ types/country.ts
@@ -0,0 +1,16 @@
+export interface CountryParams {
+  id: string;
+}
+
+export interface Country {
+  iso_code: string;
+  name: string;
+  adm0_label?: string | null;
+  adm1_label?: string | null;
+  adm2_label?: string | null;
+  adm3_label?: string | null;
+  adm4_label?: string | null;
+  adm5_label?: string | null;
+  dataset_sources?: any;
+  extra_metadata?: any;
+}
