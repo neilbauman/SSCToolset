1fc86e32a4cd90b84ed4589814575a0b268c6a99
Neil Bauman <nbauman@rocketmail.com>
2025-10-06 11:35:18 -0300

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,7 @@
+// Unified Types Index
+// -------------------
+// Always import types from "@/types" (never from sub-files directly)
+
 export * from "./country";
 export * from "./gis";
+export * from "./next.d";
