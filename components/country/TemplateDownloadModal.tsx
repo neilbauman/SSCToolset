2b1f385ecfd07c44ef4054a195ed019831f4e14c
Neil Bauman <nbauman@rocketmail.com>
2025-10-11 20:19:01 -0300

    Update TemplateDownloadModal.tsx

--- components/country/TemplateDownloadModal.tsx
+++ components/country/TemplateDownloadModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/lib/supabaseClient";
 import { Info } from "lucide-react";
 
 interface TemplateDownloadModalProps {
