a55f2400f0d64d4bd1994c458dcd49da3f91e1ae
Neil Bauman <nbauman@rocketmail.com>
2025-10-15 13:56:39 -0300

    Rename AddIndicatorModalNEW to AddIndicatorModal

--- app/configuration/indicators/page.tsx
+++ app/configuration/indicators/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { supabaseBrowser as supabase } from "@/lib/supabase/supabaseBrowser";
 import SidebarLayout from "@/components/layout/SidebarLayout";
 import Breadcrumbs from "@/components/ui/Breadcrumbs";
-import AddIndicatorModalNEW from "@/components/configuration/indicators/AddIndicatorModal";
+import AddIndicatorModal from "@/components/configuration/indicators/AddIndicatorModal";
 import EditIndicatorModal from "@/components/configuration/indicators/EditIndicatorModal";
 import {
   Plus,
