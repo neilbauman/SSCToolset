87a9a001991e16f065ae96a1591d1dadcac22676
Neil Bauman <nbauman@rocketmail.com>
2025-10-16 13:43:51 -0300

    Update page.tsx

--- app/country/[id]/datasets/add/page.tsx
+++ app/country/[id]/datasets/add/page.tsx
@@ -1,6 +1,6 @@
 // Route: /country/[id]/datasets/add
 import DatasetWizard from "../DatasetWizard";
 
-export default function AddDatasetPage({ params }: { params: { id: string } }) {
+export default function AddDatasetPage({ params }: { params: { id?: string } }) {
   return <DatasetWizard params={params} />;
 }
